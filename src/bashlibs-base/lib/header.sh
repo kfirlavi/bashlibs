c4cd4dcdd743b85c9eac9636a783f5d93c5e3dbb
Kfir Lavi <lavi.kfir@gmail.com>
2014-02-23 18:13:33 +0200

    bashlibs-base-0.0.9 fixed echo interpret -n as its argument and not as part of args
    when runnin a program with args '-n abc'
    $(args) will return 'abc' and not '-n abc' as it should.
    This is because echo will use -n as it's argument.

--- src/bashlibs-base/lib/header.sh
+++ src/bashlibs-base/lib/header.sh
@@ -4,18 +4,18 @@ export readonly _WORKING_DIR=$(pwd)
 export readonly _ARGS="$@"
 
 progname() {
-    echo $_PROGNAME
+    echo "$_PROGNAME"
 }
 
 progdir() {
-    echo $_PROGDIR
+    echo "$_PROGDIR"
 }
 
 working_directory() {
-    echo $_WORKING_DIR
+    echo "$_WORKING_DIR"
 }
 
 args() {
-    echo $_ARGS
+    echo "$_ARGS"
 }
 
