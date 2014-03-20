aac3182ee3a19e8d9b02319cf41ace4ab3994fd7
Kfir Lavi <lavi.kfir@gmail.com>
2014-03-20 14:40:45 +0200

    cmake-macros-0.0.5 added os detection support for cmake

--- src/cmake-macros/Modules/OSDetection.cmake
+++ src/cmake-macros/Modules/OSDetection.cmake
@@ -8,3 +8,24 @@ IF(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
 	add_definitions(-DLINUX)
 	add_definitions(-DLinux)
 ENDIF(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+
+execute_process(COMMAND "bashlibs" "--include os_detection.sh --run is_ubuntu"
+    RESULT_VARIABLE LINUX_UBUNTU)
+
+execute_process(COMMAND "bashlibs" "--include os_detection.sh --run is_gentoo"
+    RESULT_VARIABLE LINUX_GENTOO)
+
+if(LINUX_UBUNTU MATCHES 0)
+    set(LINUX_DISTRO ubuntu)
+    add_definitions(-DLINUX_UBUNTU)
+endif(LINUX_UBUNTU MATCHES 0)
+
+if(LINUX_GENTOO MATCHES 0)
+    set(LINUX_DISTRO gentoo)
+    add_definitions(-DLINUX_GENTOO)
+endif(LINUX_GENTOO MATCHES 0)
+
+if(LINUX_DISTRO)
+    message(STATUS "Linux distro is ${LINUX_DISTRO}")
+    message(STATUS "Defining variable: LINUX_DISTRO=${LINUX_DISTRO}")
+endif(LINUX_DISTRO)
