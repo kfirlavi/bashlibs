76063dd75f79559d067f2343051aeae679bbdcb5
Kfir Lavi <lavi.kfir@gmail.com>
2015-09-02 11:32:16 +0300

    directories-0.0.3 fixed test failure

--- src/bashlibs-directories/lib/directories.sh
+++ src/bashlibs-directories/lib/directories.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+dir_exist() {
+    local dir=$1
+
+    [[ -d $dir ]]
+}
+
 create_dir_if_needed() {
     local dir=$1
 
--- src/bashlibs-directories/test/test_directories.sh
+++ src/bashlibs-directories/test/test_directories.sh
@@ -13,12 +13,13 @@ test_dir_exist() {
 
 test_create_dir_if_needed() {
     local dir=$(mktemp -d)
+    rmdir $dir
 
-    return_false "[[ -d $dir ]]"
+    return_false "dir_exist $dir"
     create_dir_if_needed $dir > /dev/null 2>&1
-    return_true "[[ -d $dir ]]"
+    return_true "dir_exist $dir"
     create_dir_if_needed $dir > /dev/null 2>&1
-    return_true "[[ -d $dir ]]"
+    return_true "dir_exist $dir"
 
     rmdir $dir
 }
