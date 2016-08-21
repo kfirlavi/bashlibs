9a66be7fdfe5f115eb4745c5d7e57d4edaa2c421
Kfir Lavi <lavi.kfir@gmail.com>
2016-08-21 13:39:09 +0300

    bashlibs-kernel-cmdline-0.0.2 added kernel_cmdline_var_provided and kernel_cmdline_flag_provided

--- src/bashlibs-kernel-cmdline/lib/kernel_cmdline.sh
+++ src/bashlibs-kernel-cmdline/lib/kernel_cmdline.sh
@@ -7,3 +7,21 @@ kernel_cmdline_value() {
         | grep "${var}=" \
         | cut -d '=' -f 2
 }
+
+kernel_cmdline_var_provided() {
+    local var=$1
+    local proc_cmdline_file=${2:-/proc/cmdline}
+
+    cat $proc_cmdline_file \
+        | sed 's/ /\n/g' \
+        | grep -q "${var}="
+}
+
+kernel_cmdline_flag_provided() {
+    local flag=$1
+    local proc_cmdline_file=${2:-/proc/cmdline}
+
+    cat $proc_cmdline_file \
+        | sed 's/ /\n/g' \
+        | grep -q "^$flag$"
+}
--- src/bashlibs-kernel-cmdline/test/test_kernel_cmdline.sh
+++ src/bashlibs-kernel-cmdline/test/test_kernel_cmdline.sh
@@ -6,7 +6,7 @@ include kernel_cmdline.sh
 oneTimeSetUp() {
     CMDLINE_FILE=$(mktemp)
 
-    echo "ip=1.2.3.4 var1=value1 var2=value2 hd=/dev/sda" > $CMDLINE_FILE
+    echo "flag1 ip=1.2.3.4 var1=value1 flag_2 var2=value2 hd=/dev/sda flag3" > $CMDLINE_FILE
 }
 
 oneTimeTearDown() {
@@ -25,6 +25,46 @@ test_kernel_cmdline_value() {
 
     returns "/dev/sda" \
         "kernel_cmdline_value hd $CMDLINE_FILE"
+}
+
+test_kernel_cmdline_var_provided() {
+    return_true \
+        "kernel_cmdline_var_provided ip $CMDLINE_FILE"
+
+    return_true \
+        "kernel_cmdline_var_provided var1 $CMDLINE_FILE"
+
+    return_true \
+        "kernel_cmdline_var_provided var2 $CMDLINE_FILE"
+
+    return_true \
+        "kernel_cmdline_var_provided hd $CMDLINE_FILE"
+
+    return_false \
+        "kernel_cmdline_var_provided var3 $CMDLINE_FILE"
+
+    return_false \
+        "kernel_cmdline_var_provided my_ip $CMDLINE_FILE"
+}
+
+test_kernel_cmdline_flag_provided() {
+    return_true \
+        "kernel_cmdline_flag_provided flag1 $CMDLINE_FILE"
+
+    return_true \
+        "kernel_cmdline_flag_provided flag_2 $CMDLINE_FILE"
+
+    return_true \
+        "kernel_cmdline_flag_provided flag3 $CMDLINE_FILE"
+
+    return_false \
+        "kernel_cmdline_flag_provided flag4 $CMDLINE_FILE"
+
+    return_false \
+        "kernel_cmdline_flag_provided var1 $CMDLINE_FILE"
+
+    return_false \
+        "kernel_cmdline_flag_provided value1 $CMDLINE_FILE"
 }
 
 # load shunit2
