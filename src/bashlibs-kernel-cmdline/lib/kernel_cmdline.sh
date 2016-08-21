eaeb0d70b26fe27472b52659f1f89e86850d0411
Kfir Lavi <lavi.kfir@gmail.com>
2016-08-21 17:23:00 +0300

    bashlibs-kernel-cmdline-0.0.3  added kernel_cmdline_missing_params and exit functions

--- src/bashlibs-kernel-cmdline/lib/kernel_cmdline.sh
+++ src/bashlibs-kernel-cmdline/lib/kernel_cmdline.sh
@@ -1,8 +1,11 @@
+kernel_cmdline() {
+    cat /proc/cmdline
+}
+
 kernel_cmdline_value() {
     local var=$1
-    local proc_cmdline_file=${2:-/proc/cmdline}
 
-    cat $proc_cmdline_file \
+    kernel_cmdline \
         | sed 's/ /\n/g' \
         | grep "${var}=" \
         | cut -d '=' -f 2
@@ -10,18 +13,43 @@ kernel_cmdline_value() {
 
 kernel_cmdline_var_provided() {
     local var=$1
-    local proc_cmdline_file=${2:-/proc/cmdline}
 
-    cat $proc_cmdline_file \
+    kernel_cmdline \
         | sed 's/ /\n/g' \
         | grep -q "${var}="
 }
 
 kernel_cmdline_flag_provided() {
     local flag=$1
-    local proc_cmdline_file=${2:-/proc/cmdline}
 
-    cat $proc_cmdline_file \
+    kernel_cmdline \
         | sed 's/ /\n/g' \
         | grep -q "^$flag$"
+}
+
+kernel_cmdline_missing_params() {
+    local params=$@
+    local i
+
+    for i in $params
+    do
+        kernel_cmdline_var_provided $i \
+            || echo $i
+    done
+}
+
+kernel_cmdline_exit_if_param_is_missing() {
+    local param=$1
+    local missing_param=$(kernel_cmdline_missing_params $param)
+
+    [[ -n $missing_param ]] \
+        && eexit "param $(color white)$param$(no_color) must be provided as a kernel command line arguments"
+}
+
+kernel_cmdline_exit_if_params_are_missing() {
+    local params=$@
+    local missing_params=$(kernel_cmdline_missing_params $params)
+
+    [[ -n $missing_params ]] \
+        && eexit "params $(color white)$params$(no_color) must be provided as a kernel command line arguments"
 }
--- src/bashlibs-kernel-cmdline/test/test_kernel_cmdline.sh
+++ src/bashlibs-kernel-cmdline/test/test_kernel_cmdline.sh
@@ -4,67 +4,71 @@ include shunit2_enhancements.sh
 include kernel_cmdline.sh
 
 oneTimeSetUp() {
-    CMDLINE_FILE=$(mktemp)
-
-    echo "flag1 ip=1.2.3.4 var1=value1 flag_2 var2=value2 hd=/dev/sda flag3" > $CMDLINE_FILE
-}
-
-oneTimeTearDown() {
-    rm -f $CMDLINE_FILE
+    kernel_cmdline() {
+        echo "flag1 ip=1.2.3.4 var1=value1 flag_2 var2=value2 hd=/dev/sda flag3"
+    }
 }
 
 test_kernel_cmdline_value() {
     returns "1.2.3.4" \
-        "kernel_cmdline_value ip $CMDLINE_FILE"
+        "kernel_cmdline_value ip"
 
     returns "value1" \
-        "kernel_cmdline_value var1 $CMDLINE_FILE"
+        "kernel_cmdline_value var1"
 
     returns "value2" \
-        "kernel_cmdline_value var2 $CMDLINE_FILE"
+        "kernel_cmdline_value var2"
 
     returns "/dev/sda" \
-        "kernel_cmdline_value hd $CMDLINE_FILE"
+        "kernel_cmdline_value hd"
 }
 
 test_kernel_cmdline_var_provided() {
     return_true \
-        "kernel_cmdline_var_provided ip $CMDLINE_FILE"
+        "kernel_cmdline_var_provided ip"
 
     return_true \
-        "kernel_cmdline_var_provided var1 $CMDLINE_FILE"
+        "kernel_cmdline_var_provided var1"
 
     return_true \
-        "kernel_cmdline_var_provided var2 $CMDLINE_FILE"
+        "kernel_cmdline_var_provided var2"
 
     return_true \
-        "kernel_cmdline_var_provided hd $CMDLINE_FILE"
+        "kernel_cmdline_var_provided hd"
 
     return_false \
-        "kernel_cmdline_var_provided var3 $CMDLINE_FILE"
+        "kernel_cmdline_var_provided var3"
 
     return_false \
-        "kernel_cmdline_var_provided my_ip $CMDLINE_FILE"
+        "kernel_cmdline_var_provided my_ip"
 }
 
 test_kernel_cmdline_flag_provided() {
     return_true \
-        "kernel_cmdline_flag_provided flag1 $CMDLINE_FILE"
+        "kernel_cmdline_flag_provided flag1"
 
     return_true \
-        "kernel_cmdline_flag_provided flag_2 $CMDLINE_FILE"
+        "kernel_cmdline_flag_provided flag_2"
 
     return_true \
-        "kernel_cmdline_flag_provided flag3 $CMDLINE_FILE"
+        "kernel_cmdline_flag_provided flag3"
 
     return_false \
-        "kernel_cmdline_flag_provided flag4 $CMDLINE_FILE"
+        "kernel_cmdline_flag_provided flag4"
 
     return_false \
-        "kernel_cmdline_flag_provided var1 $CMDLINE_FILE"
+        "kernel_cmdline_flag_provided var1"
 
     return_false \
-        "kernel_cmdline_flag_provided value1 $CMDLINE_FILE"
+        "kernel_cmdline_flag_provided value1"
+}
+
+test_kernel_cmdline_missing_params() {
+    returns_empty \
+        "kernel_cmdline_missing_params ip var1 var2 hd"
+
+    returns "var4 aaa var3" \
+        "kernel_cmdline_missing_params var4 ip var1 var2 aaa hd var3"
 }
 
 # load shunit2
