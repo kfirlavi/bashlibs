96b7efa3b3289f057fb515403323776e0f450adb
Kfir Lavi <lavi.kfir@gmail.com>
2017-04-06 19:36:39 +0300

    bashlibs-config-0.0.2 added load_config_if_exist and config_exist

--- src/bashlibs-config/lib/config.sh
+++ src/bashlibs-config/lib/config.sh
@@ -1,8 +1,19 @@
+config_exist() {
+    local conf_file=$1
+
+    [[ -f $conf_file ]]
+}
+
+load_config_if_exist() {
+    local conf_file=$1
+
+    config_exist $conf_file \
+        && source $conf_file
+}
+
 load_config() {
     local conf_file=$1
 
-    [[ -f $conf_file ]] \
+    load_config_if_exist $conf_file \
         || eexit "$conf_file does not exist"
-
-    source $conf_file
 }
--- src/bashlibs-config/test/test_config.sh
+++ src/bashlibs-config/test/test_config.sh
@@ -11,14 +11,22 @@ delete_conf_file() {
     rm -f $(conf_file)
 }
 
-oneTimeSetUp() {
+setUp() {
+	VAR_IN_CONF_FILE=
+
 	cat<<-EOF > $(conf_file)
 	VAR_IN_CONF_FILE=123
 	EOF
 }
 
-oneTimeTearDown() {
+tearDown() {
     delete_conf_file
+}
+
+test_config_exist() {
+    return_true "config_exist $(conf_file)"
+    delete_conf_file
+    return_false "config_exist $(conf_file)"
 }
 
 test_load_config() {
@@ -28,6 +36,16 @@ test_load_config() {
 
     delete_conf_file
     return_false "load_config $(conf_file)"
+
+}
+
+test_load_config_if_exist() {
+    var_is_not_defined    VAR_IN_CONF_FILE
+    load_config $(conf_file)
+    var_should_be_defined VAR_IN_CONF_FILE
+
+    delete_conf_file
+    return_false "load_config_if_exist $(conf_file)"
 
 }
 
