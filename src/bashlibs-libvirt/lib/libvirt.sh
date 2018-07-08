3b4a2b4c675dbce5ed3a1cbd8d79497a57e35c88
Kfir Lavi <lavi.kfir@gmail.com>
2018-07-08 18:52:57 +0300

    bashlibs-libvirt-0.0.2 add vm_ip to fetch dhcp ip of vm

--- src/bashlibs-libvirt/lib/libvirt.sh
+++ src/bashlibs-libvirt/lib/libvirt.sh
@@ -5,3 +5,50 @@ fqdn_to_mac() {
         | md5sum \
         | sed 's/^\(..\)\(..\)\(..\)\(..\)\(..\).*$/54:\1:\2:\3:\4:\5/'
 }
+
+virsh_dumpxml() {
+    local vm_name=$1
+    
+    virsh dumpxml $vm_name
+}
+
+virsh_domiflist() {
+    local vm_name=$1
+    
+    virsh domiflist $vm_name
+}
+
+vm_bridge() {
+    local vm_name=$1
+    
+    virsh_dumpxml $vm_name \
+        | grep network \
+        | grep bridge \
+        | cut -d "'" -f 4
+}
+
+vm_mac() {
+    local vm_name=$1
+
+    virsh_domiflist $vm_name \
+        | grep ':' \
+        | awk '{print $5}'
+}
+
+libvirt_var_dir() {
+    echo /var/lib/libvirt
+}
+
+libvirt_dhcp_leases_file() {
+    local vm_name=$1
+
+    echo $(libvirt_var_dir)/dnsmasq/$(vm_bridge $vm_name).status
+}
+
+vm_ip() {
+    local vm_name=$1
+
+    grep -B 1 "\"mac-address\": \"$(vm_mac $vm_name)\"" $(libvirt_dhcp_leases_file $vm_name) \
+        | grep ip-address \
+        | cut -d '"' -f 4
+}
--- src/bashlibs-libvirt/test/test_libvirt.sh
+++ src/bashlibs-libvirt/test/test_libvirt.sh
@@ -9,5 +9,61 @@ test_fqdn_to_mac() {
     returns '54:cc:51:15:e6:77' 'fqdn_to_mac vm_name'
 }
 
+virsh_dumpxml() {
+	cat<<-EOF
+	<interface type='network'>
+	  <mac address='54:b1:04:34:50:5c'/>
+	  <source network='default' bridge='virbr0'/>
+	  <target dev='vnet2'/>
+	  <model type='rtl8139'/>
+	  <alias name='net0'/>
+	  <address type='pci' domain='0x0000' bus='0x00' slot='0x03' function='0x0'/>
+	</interface>
+	EOF
+}
+
+virsh_domiflist() {
+	cat<<-EOF
+	Interface  Type       Source     Model       MAC
+	-------------------------------------------------------
+	vnet2      network    default    rtl8139     54:c1:e4:35:52:8d
+	
+	EOF
+}
+
+virbr0_status() {
+	cat<<-EOF
+	[
+	    {
+	        "ip-address": "192.168.122.34",
+	        "mac-address": "54:c1:e4:35:52:8d",
+	        "hostname": "vm1",
+	        "client-id": "01:54:c1:e4:35:52:8d",
+	        "expiry-time": 1531063375
+	    }
+	]
+	EOF
+}
+
+test_vm_bridge() {
+    returns virbr0 "vm_bridge vm1"
+}
+
+test_vm_mac() {
+    returns '54:c1:e4:35:52:8d' "vm_mac vm1"
+}
+
+test_libvirt_dhcp_leases_file() {
+    returns '/var/lib/libvirt/dnsmasq/virbr0.status' \
+        "libvirt_dhcp_leases_file vm1"
+}
+
+test_vm_ip() {
+    virbr0_status > /tmp/status
+    libvirt_dhcp_leases_file() { echo /tmp/status; }
+    returns "192.168.122.34" "vm_ip vm1"
+    rm -f /tmp/status
+}
+
 # load shunit2
 source /usr/share/shunit2/shunit2
