9bd55e4999e01a05236381e2736f8ecbff856696
Kfir Lavi <lavi.kfir@gmail.com>
2020-11-02 11:57:43 +0200

    bashlibs-sysfs-0.0.7 check if sys path actually exist

--- src/bashlibs-sysfs/lib/sysfs.sh
+++ src/bashlibs-sysfs/lib/sysfs.sh
@@ -1,11 +1,15 @@
+include checks.sh
+
 sysfs_option_on() {
     local sysfs_path=$1
 
-    echo 1 > $sysfs_path
+    file_exist $sysfs_path \
+        && echo 1 > $sysfs_path
 }
 
 sysfs_option_off() {
     local sysfs_path=$1
 
-    echo 0 > $sysfs_path
+    file_exist $sysfs_path \
+        && echo 0 > $sysfs_path
 }
