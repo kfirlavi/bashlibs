c56a70a3e21200dcad751c44d2d7397a3949addd
Kfir Lavi <lavi.kfir@gmail.com>
2021-07-27 09:00:22 +0300

    bashlibs-checksum-0.0.6 added create_md5_for_all_files_in_directory

--- src/bashlibs-checksum/lib/checksum.sh
+++ src/bashlibs-checksum/lib/checksum.sh
@@ -7,3 +7,13 @@ create_md5() {
     md5sum $name > $name.md5
     cd - > /dev/null 2>&1
 }
+
+create_md5_for_all_files_in_directory() {
+    local dir=$1
+
+    local i
+    for i in $(find $dir -maxdepth 1 -type f)
+    do
+        create_md5 $i
+    done
+}
--- src/bashlibs-checksum/test/test_checksum.sh
+++ src/bashlibs-checksum/test/test_checksum.sh
@@ -2,17 +2,21 @@
 $(bashlibs --load-base)
 include shunit2_enhancements.sh
 include checksum.sh
+include directories.sh
 
 tempfile() {
-    echo /tmp/bashlibs_checksum_test_tmpfile
+    echo $(workdir)/bashlibs_checksum_test_tmpfile
 }
 
 setUp() {
+    create_workdir
     echo -n > $(tempfile)
+    echo 1 > $(workdir)/a
+    echo 2 > $(workdir)/b
 }
 
 tearDown() {
-    rm -f $(tempfile)
+    remove_workdir
 }
 
 test_create_md5() {
@@ -23,8 +27,20 @@ test_create_md5() {
     returns \
         "700ee18e43340071b8576a69e3ada352  $(basename $(tempfile))" \
         "cat $(tempfile).md5"
+}
 
-    rm -f $(tempfile).md5
+test_create_md5_for_all_files_in_directory() {
+    create_md5_for_all_files_in_directory $(workdir)
+    file_should_exist $(workdir)/a.md5
+    file_should_exist $(workdir)/b.md5
+    
+    returns \
+        "b026324c6904b2a9cb4b88d6d61c81d1  a" \
+        "cat $(workdir)/a.md5"
+
+    returns \
+        "26ab0db90d72e28ad0ba1e22ee510510  b" \
+        "cat $(workdir)/b.md5"
 }
 
 # load shunit2
