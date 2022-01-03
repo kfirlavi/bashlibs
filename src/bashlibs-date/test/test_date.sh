60eb8fc3b9a8510be4c79f37558dfa71149b2892
Kfir Lavi <lavi.kfir@gmail.com>
2022-01-03 08:55:11 +0200

    bashlibs-date-0.0.10 using mockup date for testing

--- src/bashlibs-date/test/test_date.sh
+++ src/bashlibs-date/test/test_date.sh
@@ -1,51 +1,62 @@
 #!/bin/bash
 $(bashlibs --load-base)
 include shunit2_enhancements.sh
+include config.sh
 include date.sh
 
+date() {
+    local format=$@
+
+    $(date_binary) -d "2022-02-03 15:30:48" $format
+}
+
+oneTimeSetUp() {
+    var_to_function date_binary $(which date)
+}
+
 test_date_str() {
-    returns "$(date +%Y.%m.%d)" \
+    returns "2022.02.03" \
         "date_str"
 }
 
 test_time_str() {
-    returns "$(date +%H.%M)" \
+    returns "15.30" \
         "time_str"
 }
 
 test_time_with_seconds_str() {
-    returns "$(date +%H.%M.%S)" \
+    returns "15.30.48" \
         "time_with_seconds_str"
 }
 
 test_date_time_str() {
-    returns "$(date +%Y.%m.%d-%H.%M)" \
+    returns "2022.02.03-15.30" \
         "date_time_str"
 }
 
 test_date_time_with_seconds_str() {
-    returns "$(date +%Y.%m.%d-%H.%M.%S)" \
+    returns "2022.02.03-15.30.48" \
         "date_time_with_seconds_str"
 
-    returns "$(date +%Y_%m_%d-%H_%M_%S)" \
+    returns "2022_02_03-15_30_48" \
         "date_time_with_seconds_str '_'"
 
-    returns "$(date +%Y_%m_%d_%H_%M_%S)" \
+    returns "2022_02_03_15_30_48" \
         "date_time_with_seconds_str '_' '_'"
 }
 
 test_date_year() {
-    returns "$(date +%Y)" \
+    returns "2022" \
         "date_year"
 }
 
 test_date_month() {
-    returns "$(date +%m)" \
+    returns "02" \
         "date_month"
 }
 
 test_date_day() {
-    returns "$(date +%d)" \
+    returns "03" \
         "date_day"
 }
 
